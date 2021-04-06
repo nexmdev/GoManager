a1324b6f3714857dc593d10bf7821d22d93163dc
Nehal <dev.nexm@gmail.com>
2021-04-06 18:30:19 +0530

    Initial commit

---
+++ app/src/main/java/com/nexm/gomanager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nexm.gomanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
